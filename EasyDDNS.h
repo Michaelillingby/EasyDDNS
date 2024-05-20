63d17bc524555d3227618d8156b8968586b294dd
Michaelillingby <Michael.i@me.com>
2024-05-20 00:05:24 -0700

    Update EasyDDNS.h
    Updated for noip.com for an esp-01 in 2024

--- EasyDDNS.h
+++ EasyDDNS.h
@@ -1,45 +1,31 @@
-/*
-EasyDDNS Library for ESP8266 or ESP32
-See the README file for more details.
-
-Written in 2017 by Ayush Sharma. Licensed under MIT.
-*/
-
 #ifndef EasyDDNS_H
 #define EasyDDNS_H
 
 #include "Arduino.h"
 #include "stdlib_noniso.h"
-
-#if defined(ESP8266)
-  #include "ESP8266WiFi.h"
-  #include "ESP8266HTTPClient.h"
-  #define HARDWARE "esp8266"
-#elif defined(ESP32)
-  #include "WiFi.h"
-  #include "HTTPClient.h"
-  #define HARDWARE "esp32"
-#endif
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
 
 // Handler to notify user about new public IP
 typedef std::function<void(const char* old_ip, const char* new_ip)> DDNSUpdateHandler;
 
-class EasyDDNSClass{
-  public:
+class EasyDDNSClass {
+public:
     void service(String ddns_service);
     void client(String ddns_domain, String ddns_username, String ddns_password = "");
     void update(unsigned long ddns_update_interval, bool use_local_ip = false);
+    String getRegisteredIP(); // New method to check the current IP registered with No-IP
 
     // Callback
     void onUpdate(DDNSUpdateHandler handler) {
-      _ddnsUpdateFunc = handler;
+        _ddnsUpdateFunc = handler;
     }
 
-  private:
+private:
     DDNSUpdateHandler _ddnsUpdateFunc = nullptr;
 
     unsigned long interval;
-    unsigned long previousMillis;
+    unsigned long previousMillis = 0;
 
     String new_ip;
     String old_ip;
@@ -49,5 +35,7 @@ class EasyDDNSClass{
     String ddns_d;
     String ddns_choice;
 };
+
 extern EasyDDNSClass EasyDDNS;
+
 #endif
